97586411c21e8db463c08e5c151827bcd06e5714
lepassant <mdstoffolano@gmail.com>
2020-12-09 21:04:36 +0000

    The inclusion as a server.

--- client.py
+++ client.py
@@ -5,6 +5,9 @@ Author:Matthew Stoffolano
 """
 import socket
 import sys
+import threading
+
+command_list = ["client_tcp", "client_udp", "server_tcp", "server_udp", "exit"]
 
 def get_target_info():
 	"""
@@ -56,18 +59,64 @@ def tcp_client():
 		client.close()
 	except:
 		print "The connection was not able to be created."
+def server_threading(connection):
+        print connection.recv(4096)
+	connection.socket()
+	connection.send("ACK")
+	connection.close()
+
+def server_threading_udp(connection):
+	print conenction.recvfrom(4096)
+	connection.sendto(bytesToSend, address)
+
+def tcp_server():
+	host, port = get_target_info()
+	server = socket.socket()
+	server.bind((host, port))
+	server.listen(5)
+	while True:
+		client, addr = server.accept()
+		connection_thread = threading.Thread(target = server_threading, args=(client,))
+		connection_thread.start()
+
+def udp_server():
+	host, port = get_target_info()
+	server =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+	server.bind((host, port))
+	while True:
+		connection_thread = threading.Thread(target = server_threading_udp, args=(server,))
+
+def help():
+	print "Commands:"
+	for i in range(0, len(command_list)):
+		print command_list[i]
 
 def user_interface():
 	"""
 	Description: This is what the user can use to decide on a function to be made.
-	"""
-	command = raw_input("> ")
-	if command == "tcp":
-		tcp_client()
-	elif command == "exit":
-		sys.exit()
-	else:
-		print "You entered " + str(command) + " ,which is a unknwon command."
+	host, port = get_target_info()
+        server = socket.socket()
+        server.bind((host, port))
+        server.listen()
+        print server.client_socket.recv(4096)
+"""
+	command = ""
+	while command != "exit":
+		command = raw_input("> ")
+		if command == "client_tcp":
+			tcp_client()
+		elif command == "client_udp":
+			udp_client()
+		elif command == "server_tcp":
+			tcp_server()
+		elif command == "server_udp":
+			udp_server()
+		elif command == "help":
+			help()
+		elif command == "exit":
+			sys.exit()
+		else:
+			print "You entered " + str(command) + " ,which is a unknwon command."
 
 def main():
 	print "Welcome to Client"
